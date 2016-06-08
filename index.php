55f29e4664df0d62288a9dabae4141c34568cb95
#manosenelcódigo <peligrosamente@gmail.com>
2016-06-08 00:55:12 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Creación de un documento TXT desde PHP</title>
+    </head>
+    <body>
+        <ul>
+            <li> <a href="crear.php">Crear</a> </li>
+            <li><a href="leer.php">Leer</a> </li>
+        </ul>
+    </body>
+    </html>
